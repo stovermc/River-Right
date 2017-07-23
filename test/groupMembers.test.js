95afe7b19bbe748da6ec3ad673797b74b2f0e127
stovermc <stovermc@gmail.com>
2017-07-23 14:54:06 -0600

    Add delete group member endpoint
    * add delete function to groupMemberRouter, controller, model

--- test/groupMembers.test.js
+++ test/groupMembers.test.js
@@ -25,6 +25,13 @@ describe('Server connection', function() {
     this.server.close()
   })
 
+  beforeEach(function(done) {
+    GroupMember.create(1, 2)
+      .then(function() {
+        GroupMember.create(2, 2)
+          .then(function() { done() })
+      })
+  })
   afterEach(function(done) {
     GroupMember.emptyGroupMemberTable()
     .then(function() { done() })
@@ -40,6 +47,18 @@ describe('Server connection', function() {
         assert.equal(response.statusCode, 200)
         assert.equal(groupMember.user_id, 1)
         assert.equal(groupMember.group_id, 1)
+        done()
+      })
+    })
+
+    it('DELETE /groupmembers', function(done) {
+      this.request.delete('/groupmembers/2', function(error, response, body) {
+        if (error) { done() }
+
+        const deletedGroupMember = JSON.parse(body)
+        assert.equal(response.statusCode, 200)
+        assert.equal(deletedGroupMember.user_id, 2)
+        assert.equal(deletedGroupMember.group_id, 2)
         done()
       })
     })
