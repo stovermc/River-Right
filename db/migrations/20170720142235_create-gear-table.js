f674292a6a9b90f1e23419470239947ba4cd74bf
stovermc <stovermc@gmail.com>
2017-07-24 17:57:59 -0600

    Refactor show gear type
    * the function now will return all the names of gear that a given user
    owns filtered by the gear_type id
    
    * adjusted route to take in multiple parameters

--- db/migrations/20170720142235_create-gear-table.js
+++ db/migrations/20170720142235_create-gear-table.js
@@ -1,6 +1,7 @@
 
 exports.up = function(knex, Promise) {
   let createQuery = `CREATE TABLE gear_items(
+    id SERIAL PRIMARY KEY NOT NULL,
     name TEXT,
     gear_type_id INT,
     created_at TIMESTAMP
